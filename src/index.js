dc03c4d13c622b5dc505a7396e34f55458be1241
enoqueJonas <enoque.macanda@gmail.com>
2022-11-02 18:42:14 +0200

    FIX linters

--- src/index.js
+++ src/index.js
@@ -1,47 +1,45 @@
-import './index.css'
-import Icon from './more.png'
+import './index.css';
+import Icon from './more.png';
 
 const todoListUl = document.querySelector('.todo-list');
-const wrapper = document.querySelector('.wrapper')
 const todosArr = [
-    {
-        'description': 'Wash the dishes',
-        'completed': false,
-        'index': 0
-    },
-    {
-        'description': 'Read a book',
-        'completed': false,
-        'index': 1
-    },
-    {
-        'description': 'Make my bed',
-        'completed': false,
-        'index': 2
-    }
-]
+  {
+    description: 'Wash the dishes',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Read a book',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Make my bed',
+    completed: false,
+    index: 2,
+  },
+];
 
 const populateTodo = () => {
-    console.log('Enter')
-    let todoItems = ` <li class="todo-item todo-title">
+  let todoItems = ` <li class="todo-item todo-title">
             <h2> Today's To Do</h2>
     </li>
     <li class="todo-item todo-add">
         <input type="text" placeholder="Add to your list...">
     </li>`;
-    todosArr.forEach((todo) => {
-        todoItems = todoItems + ` <li class="todo-item todos">
+  todosArr.forEach((todo) => {
+    todoItems = `${todoItems} <li class="todo-item todos">
         <div class="todo-wrap">
             <input type="checkbox">
             <p class="todo-text"> ${todo.description}</p>
         </div>
         <img src="${Icon}" alt="" class="todo-hold">
-        </li>`
-    })
-    todoItems = todoItems + ` <li class="todo-item todo-button">
+        </li>`;
+  });
+  todoItems = `${todoItems} <li class="todo-item todo-button">
     <button>Clear all completed</button>
-</li>`
-    todoListUl.innerHTML = todoItems;
-}
+</li>`;
+  todoListUl.innerHTML = todoItems;
+};
 
-window.addEventListener('load', populateTodo())
+window.addEventListener('load', populateTodo());
