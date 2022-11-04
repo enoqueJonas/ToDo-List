a2afef85cf95df258d2e577b793ad6e1868a2fac
enoqueJonas <enoque.macanda@gmail.com>
2022-11-04 16:50:31 +0200

    IMPROVE html page structure

--- src/index.js
+++ src/index.js
@@ -1,47 +1,57 @@
 import './index.css';
 import Icon from './more.png';
 import Refresh from './refresh.png';
+import Edit from './edit.png';
+import Remove from './remove.png';
+import Add from './plus.png';
+import { todosArr } from './todoArray.js';
+import Todo from "./Todo.js";
 
+const todoAddInput = document.querySelector('#todo-add-input')
+const refresImg = document.querySelector('#refresh');
+const btnClear = document.querySelector('#btn-clear');
 const todoListUl = document.querySelector('.todo-list');
-const todosArr = [
-  {
-    description: 'Wash the dishes',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Read a book',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Make my bed',
-    completed: false,
-    index: 2,
-  },
-];
+const btnAddTodo = document.querySelector('#btn-add-todo')
 
 const populateTodo = () => {
-  let todoItems = ` <li class="todo-item todo-title">
-            <h2> Today's To Do</h2>
-            <img src=${Refresh} alt="refresh" class="todo-hold">
-    </li>
-    <li class="todo-item todo-add">
-        <input type="text" placeholder="Add to your list...">
-    </li>`;
-  todosArr.forEach((todo) => {
-    todoItems = `${todoItems} <li class="todo-item todos">
+    let todoItems = '';
+    refresImg.src = Refresh;
+    btnAddTodo.src = Add;
+    todosArr.forEach((todo, index) => {
+        todoItems = `${todoItems} <li id="${index}" class="todo-item todos">
         <div class="todo-wrap">
             <input type="checkbox">
-            <p class="todo-text"> ${todo.description}</p>
+            <input type="text" class="todo-text" value="${todo.description}">
         </div>
+        <img src="${Edit}" alt="edit" class="todo-hold">
+        <img src="${Remove}" alt="edit" class="todo-hold">
         <img src="${Icon}" alt="move" class="todo-hold">
         </li>`;
-  });
-  todoItems = `${todoItems} <li class="todo-item todo-button">
-    <button>Clear all completed</button>
+    });
+    todoItems = `${todoItems} <li class="todo-item todo-button">
+    <button id="btn-clear">Clear all completed</button>
 </li>`;
-  todoListUl.innerHTML = todoItems;
+    todoListUl.innerHTML = todoItems;
 };
 
+const addTodo = () => {
+    let todotext = todoAddInput.value;
+    let todo = new Todo(todotext, false, todosArr.length);
+    todosArr.push(todo);
+    populateTodo();
+    return todosArr;
+}
+
+const removeTodo = () => {
+
+}
+
 window.onload = populateTodo();
+
+btnAddTodo.addEventListener('click', addTodo);
+
+if (btnClear) {
+    btnClear.addEventListener('clear', () => {
+        console.log('Enter')
+    })
+}
---
+++ src/todoArray.js
@@ -0,0 +1,17 @@
+export const todosArr = [
+    {
+        description: 'Wash the dishes',
+        completed: false,
+        index: 0,
+    },
+    {
+        description: 'Read a book',
+        completed: false,
+        index: 1,
+    },
+    {
+        description: 'Make my bed',
+        completed: false,
+        index: 2,
+    },
+];
