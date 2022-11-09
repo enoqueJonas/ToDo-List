940e9f56f9222f5cb0377af820308816ff3546cd
enoqueJonas <enoque.macanda@gmail.com>
2022-11-09 18:19:45 +0200

    IMPROVE clear completed functionality, allowing it to delete multiple tasks

--- src/index.js
+++ src/index.js
@@ -16,32 +16,26 @@ const btnAddTodo = document.querySelector('#btn-add-todo');
 let editTodo = {};
 
 const updateTasks = (event) => {
-    const eventItemId = event.target.id;
-    const matches = eventItemId.replace(/^\D+/g, '');
-    const editId = Number(matches);
-    tasksArr.forEach((task) => {
-        if (editId === task.index && event.target.checked) {
-            task.completed = true;
-        }else{
-            task.completed = false;
-        }
-    });
-    console.log(tasksArr)
-};
-
-const addBtnClearEvent = () => {
-    const btnClear = document.querySelector('#btn-clear');
-    btnClear.addEventListener('click', clearCompleted);
+  const eventItemId = event.target.id;
+  const matches = eventItemId.replace(/^\D+/g, '');
+  const editId = Number(matches);
+  tasksArr.forEach((task) => {
+    if (editId === task.index && event.target.checked === true) {
+      task.completed = true;
+    } else if(editId === task.index && event.target.checked !== true){
+      task.completed = false;
+    }
+  });
 };
 
 const populateTodo = () => {
-    let todoItems = '';
-    refresImg.src = Refresh;
-    btnAddTodo.src = Add;
-    const storageTasks = JSON.parse(localStorage.getItem('todos')) || [];
-    if (tasksArr !== null) {
-        storageTasks.forEach((todo, index) => {
-            todoItems = `${todoItems} <li class="todo-item todos">
+  let todoItems = '';
+  refresImg.src = Refresh;
+  btnAddTodo.src = Add;
+  const storageTasks = JSON.parse(localStorage.getItem('todos')) || [];
+  if (tasksArr !== null) {
+    storageTasks.forEach((todo, index) => {
+      todoItems = `${todoItems} <li class="todo-item todos">
           <div class="todo-wrap">
               <input id="check-${index + 1}" class="checkboxes" type="checkbox">
               <input id="todo-${index + 1}" type="text" class="todo-text" value="${todo.description}">
@@ -50,79 +44,86 @@ const populateTodo = () => {
           <img src="${Remove}" id="${index + 1}" alt="edit" class="remove-btn todo-hold">
           <img src="${Icon}" alt="move" class="todo-hold">
           </li>`;
-        });
-    }
-    todoItems = `${todoItems} <li class="todo-item todo-button">
+    });
+  }
+  todoItems = `${todoItems} <li class="todo-item todo-button">
       <button id="btn-clear">Clear all completed</button>
   </li>`;
-    todoListUl.innerHTML = todoItems;
-    addCompletedEvents(updateTasks);
-    addBtnClearEvent();
+  todoListUl.innerHTML = todoItems;
+  addCompletedEvents(updateTasks);
+  addBtnClearEvent();
 };
 
+const addBtnClearEvent = () => {
+    const btnClear = document.querySelector('#btn-clear');
+    btnClear.addEventListener('click', clearCompleted);
+  };
+
 const clearCompleted = () => {
-    const filteredTasks = tasksArr.filter((task) => task.completed === false);
-    filteredTasks.forEach((task, index) => {
-        task.index = index + 1;
-    });
-    localStorage.setItem('todos', JSON.stringify(filteredTasks));
-    populateTodo();
+  const filteredTasks = tasksArr.filter((task) => task.completed === false);
+  filteredTasks.forEach((task, index) => {
+    task.index = index + 1;
+  });
+  localStorage.setItem('todos', JSON.stringify(filteredTasks));
+  populateTodo();
 };
 
 const removeTodo = (event) => {
-    const btnId = Number(event.target.id);
-    tasksArr.splice(btnId - 1, 1);
-    tasksArr.forEach((todo, index) => {
-        todo.index = index + 1;
-    });
-    localStorage.setItem('todos', JSON.stringify(tasksArr));
-    populateTodo();
-    addDeleteEvents(removeTodo);
-    addEditEvents(editTodo);
+  const btnId = Number(event.target.id);
+  tasksArr.splice(btnId - 1, 1);
+  tasksArr.forEach((todo, index) => {
+    todo.index = index + 1;
+  });
+  localStorage.setItem('todos', JSON.stringify(tasksArr));
+  populateTodo();
+  addDeleteEvents(removeTodo);
+  addEditEvents(editTodo);
 };
 
 editTodo = (event) => {
-    const todoTextInput = document.querySelectorAll('.todo-text');
-    const eventItemId = event.target.id;
-    let newDescription = '';
-    const matches = eventItemId.replace(/^\D+/g, '');
-    const editId = Number(matches);
-    const todoTextInputArr = Array.from(todoTextInput);
-    todoTextInputArr.forEach((input) => {
-        const inputId = input.id;
-        const extractId = Number(inputId.replace(/^\D+/g, ''));
-        if (editId === extractId) {
-            newDescription = input.value;
-        }
-    });
-    tasksArr.forEach((todo, index) => {
-        if (editId - 1 === index) {
-            todo.description = newDescription;
-        }
-    });
-    localStorage.setItem('todos', JSON.stringify(tasksArr));
-    populateTodo();
-    addDeleteEvents(removeTodo);
-    addEditEvents(editTodo);
+  const todoTextInput = document.querySelectorAll('.todo-text');
+  const eventItemId = event.target.id;
+  let newDescription = '';
+  const matches = eventItemId.replace(/^\D+/g, '');
+  const editId = Number(matches);
+  const todoTextInputArr = Array.from(todoTextInput);
+  todoTextInputArr.forEach((input) => {
+    const inputId = input.id;
+    const extractId = Number(inputId.replace(/^\D+/g, ''));
+    if (editId === extractId) {
+      newDescription = input.value;
+    }
+  });
+  tasksArr.forEach((todo, index) => {
+    if (editId - 1 === index) {
+      todo.description = newDescription;
+    }
+  });
+  localStorage.setItem('todos', JSON.stringify(tasksArr));
+  populateTodo();
+  addDeleteEvents(removeTodo);
+  addEditEvents(editTodo);
 };
 
 const addTodo = () => {
-    const todotext = todoAddInput.value;
-    const storageTasks = JSON.parse(localStorage.getItem('todos'));
-    tasksArr.splice(0, tasksArr.length, ...storageTasks)
-    const todo = new Todo(todotext, false, tasksArr.length + 1);
-    tasksArr.push(todo);
-    todoAddInput.value = '';
-    localStorage.setItem('todos', JSON.stringify(tasksArr));
-    populateTodo();
-    addDeleteEvents(removeTodo);
-    addEditEvents(editTodo);
+  const todotext = todoAddInput.value;
+  const storageTasks = JSON.parse(localStorage.getItem('todos'));
+  if(tasksArr !== []){
+    tasksArr.splice(0, tasksArr.length, ...storageTasks);
+  }
+  const todo = new Todo(todotext, false, tasksArr.length + 1);
+  tasksArr.push(todo);
+  todoAddInput.value = '';
+  localStorage.setItem('todos', JSON.stringify(tasksArr));
+  populateTodo();
+  addDeleteEvents(removeTodo);
+  addEditEvents(editTodo);
 };
 
 const loadPage = () => {
-    populateTodo();
-    addDeleteEvents(removeTodo);
-    addEditEvents(editTodo);
+  populateTodo();
+  addDeleteEvents(removeTodo);
+  addEditEvents(editTodo);
 };
 
 window.onload = loadPage();
